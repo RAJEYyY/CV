647be3777a479273dd10b175ced8c9c9d3f6883c
RAJEYyY <98945323+RAJEYyY@users.noreply.github.com>
2022-05-25 15:55:49 +0200

    Add files via upload

---
+++ CV.js
@@ -0,0 +1,24 @@
+let mail = [
+    'vaz' ,
+    'quez' ,
+    'tor' , 
+    'res' ,
+    'die' ,
+    'go' ,
+    'pro' ,
+    '@' ,
+    'gma' ,
+    'il' ,
+    '.co' ,
+    'm' ,
+]
+document.querySelector('#mail').innerText = mail.join('')
+let tel = [
+'83' ,
+'54' ,
+'48' ,
+'17' ,
+'06' ,
+]
+document.querySelector('#tel').innerText = tel.reverse().join(' ')
+
